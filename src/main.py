82aea6f28fbe60453bfd92a9b37845eb9bd3e0aa
Jisan Mahmud <jisa.mahmud20@gmail.com>
2025-11-26 11:48:25 +0600

    use lifespan instead of on_event

--- src/main.py
+++ src/main.py
@@ -10,10 +10,24 @@ from .logging_config import setup_logging
 
 setup_logging()
 
+
+async def lifespan(app: FastAPI):
+    await init_db() # db initialize
+
+    # redis and rate limiter setup
+    r = redis.from_url("redis://localhost:6379", decode_responses=True)
+    await FastAPILimiter.init(r)
+
+    yield
+
+    await close_db() # close db connection
+
 app = FastAPI(
     title="FastAPI Book Backend",
-    version="1.0.0"
+    version="1.0.0",
+    lifespan= lifespan
 )
+
 
 # Include routers
 app.include_router(book_router, prefix="/books", tags=["book"])
@@ -23,17 +37,3 @@ app.include_router(auth_router, prefix="/auth", tags=["auth"])
 
 # add pagination support globally
 add_pagination(app)
-
-# Startup / Shutdown events
-@app.on_event("startup")
-async def on_startup():
-    await init_db() # db initialize
-
-    # redis and rate limiter setup
-    # Connect to Redis - typically you'd get this from environment variables
-    r = redis.from_url("redis://localhost:6379", decode_responses=True)
-    await FastAPILimiter.init(r)
-
-@app.on_event("shutdown")
-async def on_shutdown():
-    await close_db()
